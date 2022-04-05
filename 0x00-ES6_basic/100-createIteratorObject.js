26c79a64efd664a7169f392f670cba27c772d893
Irene-Busah <i.busah@alustudent.com>
2022-04-05 22:10:57 +0200

    createIteratorObject

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,7 +2,7 @@ export default function createIteratorObject(report) {
 	const employees = [];
 	for (const dept in report.allEmployees){
 		if ({}.hasOwnProperty.call(report.allEmployees)){
-			employees.push(...report.allEmployees[dept])
+			employees.push(...report.allEmployees[dept]);
 		}
 	}
 	return employees;
