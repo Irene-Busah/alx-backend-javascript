208c189e0938d8d936ebced08c8e1eb40e1a39da
Irene-Busah <i.busah@alustudent.com>
2022-04-07 21:57:32 +0200

    holbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,52 +1,17 @@
 export default class HolbertonCourse {
-  constructor(name, length, students){
+  constructor(name, length, students) {
     HolbertonCourse.checkType(name, 'string', 'Name must be a string');
     this._name = name;
 
     HolbertonCourse.checkType(length, 'number', 'Length must be a number');
     this._length = length;
 
-    if (Array.isArray(students)){
-      for (const element of students){
+    if (Array.isArray(students)) {
+      for (const element of students) {
         HolbertonCourse.checkType(
           element,
           'string',
-          'Student element not a string',
-        );
-      }
-      this._students = students;
-  } else {
-    throw TypeError('Students must be an array');
-    }
-  }
-
-  // Getter methods
-  get name() {
-    return this._name;
-  }
-  set name(newName) {
-    HolbertonCourse.checkType(newName, 'string', 'Name must be a string');
-    this._name = newName;
-  }
-
-  get length() {
-    return this._length;
-  }
-  set length(newLength) {
-    HolbertonCourse.checkType(newLength, 'number', 'Length must be a number');
-    this._length = newLength;
-  }
-
-  get students() {
-    return this._students;
-  }
-  set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      for (const element of newStudents){
-        HolbertonCourse.checkType(
-          element,
-          'string',
-          'Students element not a string',
+          'Student elementent not a string',
         );
       }
       this._students = students;
@@ -54,6 +19,46 @@ export default class HolbertonCourse {
       throw TypeError('Students must be an array');
     }
   }
+
+  // Getter methods
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Settor methods
+  set name(newName) {
+    HolbertonCourse.checkType(newName, 'string', 'Name must be a string');
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    HolbertonCourse.checkType(newLength, 'number', 'Length must be a number');
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      for (const element of newStudents) {
+        HolbertonCourse.checkType(
+          element,
+          'string',
+          'Student elementent not a string',
+        );
+      }
+      this._students = newStudents;
+    } else {
+      throw TypeError('Students must be an array');
+    }
+  }
+
   // Static utility method
   static checkType(arg, type, errMsg) {
     /*
