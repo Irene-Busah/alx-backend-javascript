b2ade9e7a2d74bcf5cde6f217e09de7577a96a2c
Irene-Busah <i.busah@alustudent.com>
2022-04-07 21:03:56 +0200

    make-classroom

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -1,4 +1,12 @@
-import ClassRoom from "./0-classroom.js";
+// import ClassRoom from "./0-classroom.js";
 
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
+// const room = new ClassRoom(10);
+// console.log(room._maxStudentsSize)
+
+
+
+// ===================== 2-make_classrooms ===================
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
+
