bd686a2a33540994528e8bdb01f2a693989cc956
Irene-Busah <i.busah@alustudent.com>
2022-04-07 21:53:00 +0200

    holbertonCourse

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,66 @@
+export default class HolbertonCourse {
+  constructor(name, length, students){
+    HolbertonCourse.checkType(name, 'string', 'Name must be a string');
+    this._name = name;
+
+    HolbertonCourse.checkType(length, 'number', 'Length must be a number');
+    this._length = length;
+
+    if (Array.isArray(students)){
+      for (const element of students){
+        HolbertonCourse.checkType(
+          element,
+          'string',
+          'Student element not a string',
+        );
+      }
+      this._students = students;
+  } else {
+    throw TypeError('Students must be an array');
+    }
+  }
+
+  // Getter methods
+  get name() {
+    return this._name;
+  }
+  set name(newName) {
+    HolbertonCourse.checkType(newName, 'string', 'Name must be a string');
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+  set length(newLength) {
+    HolbertonCourse.checkType(newLength, 'number', 'Length must be a number');
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      for (const element of newStudents){
+        HolbertonCourse.checkType(
+          element,
+          'string',
+          'Students element not a string',
+        );
+      }
+      this._students = students;
+    } else {
+      throw TypeError('Students must be an array');
+    }
+  }
+  // Static utility method
+  static checkType(arg, type, errMsg) {
+    /*
+      * Throw error if `arg` is not of type `type`
+      */
+    // eslint-disable-next-line valid-typeof
+    if (typeof arg !== type) throw TypeError(errMsg);
+    return true;
+  }
+}
