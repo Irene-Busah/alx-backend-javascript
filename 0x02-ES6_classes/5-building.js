a7c7413d3737b17733a3ea2d5b244f5e0ff4127c
Irene-Busah <i.busah@alustudent.com>
2022-04-08 01:56:13 +0200

    pricing

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,18 @@
-/* Eslint-disabled */
+/* eslint-disable */
+
 export default class Building {
   constructor (sqft) {
-    this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-      throw Error('Class extending Building must override evacuationWarningMessage');
-    }
+      this._sqft = sqft;
+      if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+          throw Error('Class extending Building must override evacuationWarningMessage');
+      }
   }
 
-  // getter method
   get sqft() {
-    return this._sqft;
+      return this._sqft;
   }
 
-  // setter method
-  set sqft(newSqrt) {
-    this._sqft = newSqrt;
+  set sqft(value) {
+      this._sqft = value;
   }
 }
