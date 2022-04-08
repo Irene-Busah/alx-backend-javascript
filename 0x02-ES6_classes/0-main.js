839067476ae75235fe5fd12e9f71b2933f7c7f69
Irene-Busah <i.busah@alustudent.com>
2022-04-08 02:52:14 +0200

    sky-high

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -75,9 +75,19 @@
 
 
 // =========================== 6-sky_high =========================
-import SkyHighBuilding from './6-sky_high.js';
+// import SkyHighBuilding from './6-sky_high.js';
 
-const building = new SkyHighBuilding(140, 60);
-console.log(building.sqft);
-console.log(building.floors);
-console.log(building.evacuationWarningMessage());
+// const building = new SkyHighBuilding(140, 60);
+// console.log(building.sqft);
+// console.log(building.floors);
+// console.log(building.evacuationWarningMessage());
+
+
+
+
+// =========================== 7-airport ==========================
+import Airport from "./7-airport.js";
+
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
