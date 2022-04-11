fbb09d8eab3c3ad7115dd56f5a8677898b2bac9a
Irene-Busah <i.busah@alustudent.com>
2022-04-12 01:28:35 +0200

    weak

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,11 @@
 export default function updateUniqueItems(groceries) {
-    if (!(groceries instanceof Map)) {
-      throw Error('Cannot process');
+  if (!(groceries instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  groceries.forEach((val, key) => {
+    if (val === 1) {
+      groceries.set(key, 100);
     }
-    groceries.forEach((val, key) => {
-      if (val === 1) {
-        groceries.set(key, 100);
-      }
-    });
-    return groceries;
+  });
+  return groceries;
   }
