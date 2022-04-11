9287624988a7a5ae4f9a66fc400c00c880e02d88
Irene-Busah <i.busah@alustudent.com>
2022-04-11 23:35:39 +0200

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) throw Error('Position outside range');
   const buffer = new ArrayBuffer(length);
