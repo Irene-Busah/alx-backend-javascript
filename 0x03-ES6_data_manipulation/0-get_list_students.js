a9bbce564292befd644c30fad17fcaed6b728dd4
Irene-Busah <i.busah@alustudent.com>
2022-04-11 15:42:57 +0200

    get student list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,5 @@
+/* Eslint-disabled */
+export default function getListStudents() {
+  const array = new Array({'id': 1, 'firstname': 'Guillaume', 'location': 'San Francisco'}, {'id': 2, 'firstname': 'James', 'location': 'Columbia'}, {'id': 3, 'firstname': 'Serena', 'location': 'San Francisco'});
+  return array;
+}
