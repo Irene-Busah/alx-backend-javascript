4226aa8198287ca6936bed538fecca819d848eb1
Irene-Busah <i.busah@alustudent.com>
2022-04-11 23:05:48 +0200

    new files

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 export default function updateStudentGradeByCity(students, city, newGrade) {
   const newArray =  students.map((student) => {
-      const studentGrade = newGrade.filter((element) => ( element.studentId === student.id ))
-                                   .map((x) => x.grade)[0];
+      const studentGrade = newGrade.filter((element) => ( element.studentId === student.id )).map((x) => x.grade)[0];
       student["grade"] = studentGrade || 'N/A';
       return student; 
   });
